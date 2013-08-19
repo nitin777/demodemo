ff66ba4620557f490434ad237501b826bf2ac25b
nitin <nitin@nitin.(none)>
2013-08-19 12:27:55 +0530

    order changed

--- app/controllers/cemeteries_controller.rb
+++ app/controllers/cemeteries_controller.rb
@@ -20,7 +20,7 @@ class CemeteriesController < ApplicationController
   # GET /cemeteries/1.json
   def show
     session[:cemetery_id] = params[:id] if params[:id]
-    redirect_to areas_url
+    redirect_to users_url
   end
 
   # GET /cemeteries/new
